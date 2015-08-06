fa1662fb27761df9c6d0c0b257f4151bb7ef9816
Cynthia <cynthia.tedesco@gmail.com>
2015-08-06 17:16:55 +0200

    ejemplos funcionando de javascript jquery en linea y jquery a traves de funciones. Problema al eliminar una linea aparentemente al pedo <script/>

--- public_html/index.php
+++ public_html/index.php
@@ -5,11 +5,16 @@ require_once(TEMPLATES_PATH."/header.php");
 
 <html>	
 <head>
-	<script type="text/javascript" src="../vendor/components/jquery/jquery.min.js"/>
+	<script src="../vendor/components/jquery/jquery.min.js"/>
+	</script>
 	<script type="text/javascript">
 		
+		$(document).ready(function(){
+			alert("uy uy uy");
+		});
+		
 		function showSomething(){
-			alert("lala");
+			alert("uy uy uy");
 		};
 		
 		function escribirBienvenida(){ 
@@ -22,11 +27,17 @@ require_once(TEMPLATES_PATH."/header.php");
 <body>
 	<div id="container">
 		<div id="content" background-color=red>
+		
+			<!-- javascript desde evento -->
 			<p>Página principal</p>
 			<button id="products" type="button" onClick="escribirBienvenida()">Click me!</button>
-			<div id="capa" style="padding: 10px; background-color: #ff8800">Haz clic en un botón</div>
-			<input type="button" value="Botón A" onclick="$('#capa').html('Has hecho clic en el botón <b>A</b>')">
-			<input type="button" value="Botón B" onclick="$('#capa').html('Recibido un clic en el botón <b>B</b>')">
+			<br /><br /><br />
+			
+			<!-- jquery in line -->
+			<div id="capa1" style="padding: 10px; background-color: #ff8800">Haz clic en un botón</div>
+			<br />
+			<input type="button" value="Botón A" onclick="$('#capa1').html('Has hecho clic en el botón <b>A</b>')">
+			<input type="button" value="Botón B" onclick="$('#capa1').html('Recibido un clic en el botón <b>B</b>')">
 		</div>
 		<?php require_once(TEMPLATES_PATH . "/rightPanel.php"); ?>
 	</div>
