168e1edeb57ce12e1dba83a5c02a076751277ff0
Cynthia <cynthia.tedesco@gmail.com>
2015-08-06 14:19:30 +0200

    jquery instalado, javascript funcionando levemente en el index

--- public_html/index.php
+++ public_html/index.php
@@ -5,53 +5,13 @@ require_once(TEMPLATES_PATH."/header.php");
 
 <html>	
 <head>
-	<script>
-	
-		/*$(document).ready(function(){
-		jAlert('This is a custom alert box', 'Alert Dialog');
-			$('#products').click(function(){
-				var clickBtnValue = $(this).val();
-				var ajaxurl = 'getProducts.php',
-				data =  {'action': clickBtnValue};
-				$.post(ajaxurl, data, function (response) {
-					// Response div goes here.
-					alert("action performed successfully");
-				});
-			});
-		});*/
-
-		/*function showSomething(){
-			if (window.XMLHttpRequest) {
-					// code for IE7+, Firefox, Chrome, Opera, Safari
-					xmlhttp = new XMLHttpRequest();
-			} else {
-				// code for IE6, IE5
-				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-			}
-			xmlhttp.onreadystatechange = function() {
-				if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-					document.getElementById("txtProducts").innerHTML = xmlhttp.responseText;
-				}
-			}
-			xmlhttp.open("GET","getProducts.php",true);
-			xmlhttp.send();
-			}*/
+	<script type="text/javascript">
 		
-		/*$document.ready($.ajax({
-			type: "POST",
-			url: 'connection.php',
-			dataType: 'json',
-			data: {functionname: 'connect', arguments: ''},
-
-			success: function (obj, textstatus) {
-						  if( !('error' in obj) ) {
-							  yourVariable = obj.result;
-						  }
-						  else {
-							  console.log(obj.error);
-						  }
-					}
-		}));*/
+		function showSomething(){
+			alert("lala");
+		};
+		
+		
 	</script>
 </head>
 <body>
