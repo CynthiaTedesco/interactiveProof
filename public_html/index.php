5538e89f60234c698d2284485fa55f7004282ef4
Cynthia <cynthia.tedesco@gmail.com>
2015-08-06 15:50:45 +0200

    ejemplo de jquery funcionando

--- public_html/index.php
+++ public_html/index.php
@@ -5,12 +5,17 @@ require_once(TEMPLATES_PATH."/header.php");
 
 <html>	
 <head>
+	<script type="text/javascript" src="../vendor/components/jquery/jquery.min.js"/>
 	<script type="text/javascript">
 		
 		function showSomething(){
 			alert("lala");
 		};
 		
+		function escribirBienvenida(){ 
+			console.log("consoleando");
+		   document.write("<H1>Hola a todos</H1>") 
+		}
 		
 	</script>
 </head>
@@ -18,7 +23,10 @@ require_once(TEMPLATES_PATH."/header.php");
 	<div id="container">
 		<div id="content" background-color=red>
 			<p>Página principal</p>
-			<button id="products" type="button" onClick="showSomething()">Click me!</button>
+			<button id="products" type="button" onClick="escribirBienvenida()">Click me!</button>
+			<div id="capa" style="padding: 10px; background-color: #ff8800">Haz clic en un botón</div>
+			<input type="button" value="Botón A" onclick="$('#capa').html('Has hecho clic en el botón <b>A</b>')">
+			<input type="button" value="Botón B" onclick="$('#capa').html('Recibido un clic en el botón <b>B</b>')">
 		</div>
 		<?php require_once(TEMPLATES_PATH . "/rightPanel.php"); ?>
 	</div>
